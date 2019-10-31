981b3df5b0dd706146f345888c53858e283111e5
Jon Oulton <jon.oulton1@gmail.com>
2019-10-31 14:33:59 -0600

    Implemented local rest-client and rest-server functionality

--- rest-client.py
+++ rest-client.py
@@ -12,25 +12,56 @@ The end-goal is to have the data stored in a GCP database
 and the file stored in a GCP file storage bucket as a .json file
 """
 
+import sys
 import json
 import requests
 import jsonpickle
 
-# Define the main parameters
-addr = "http://localhost:5000/api/json_post_data_request"
-filename = "Database/example_data.json"
+# Get the arguments
+args = sys.argv
 
-# Read in the data to be sent
-jsonDataDict = json.load(open(filename))
-print(type(jsonDataDict['data0']))
-print(jsonDataDict['data0'])
-print()
-# Send data to the server
-print("Beginning to send data to the server")
-for i in range(len(jsonDataDict)):
-    print()
-    print("Sending {} POST request to server".format(jsonDataDict['data{}'.format(i)]))
-    response = requests.post(addr, data=jsonpickle.encode(jsonDataDict['data{}'.format(i)]))
-    print("Response from server: {}".format(response))
 
-print()
+class ClientApp:
+    def __init__(self, file_to_send="Database/example_data.json"):
+        self.file_to_send_as_CLA = False
+        self.server_address_as_CLA = False
+        self.server_address = "http://localhost:5000/api/json_post_data_request"
+        self.file_to_send = file_to_send
+
+    def _get_arguments(self):
+        """
+        Intended usage:
+        FOR SERVER ON LOCALHOST:
+        python3 rest-client.py
+
+        FOR REMOTE SERVER
+        python3 rest-client.py server_rest_address
+
+        Note: Server_rest_address should be JUST the IP address
+        :return:
+        """
+        raise NotImplementedError
+        # args = sys.argv
+        # if len(args) == 0:
+        #     return "localhost"
+        # if len(args) == 1:
+        #     return
+
+    def client_main(self):
+        # Read in the data to be sent
+        print("Reading data from [{}] into a dictionary".format(self.file_to_send))
+        json_data_dict = json.load(open(self.file_to_send))
+        print()
+
+        # Send data to the server
+        print("Beginning to send data to the server")
+        for i in range(len(json_data_dict)):
+            print()
+            print("Sending {} POST request to server".format('data{}'.format(i)))
+            response = requests.post(self.server_address, data=jsonpickle.encode(json_data_dict['data{}'.format(i)]))
+            print("Response from [{}]: {}".format(self.server_address, response))
+        print()
+
+
+if __name__ == "__main__":
+    ClientApp().client_main()
